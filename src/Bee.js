efa675811e9b378d05061a7d97c3442e67296cd3
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-02-25 15:30:31 -0800

    solved

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,16 @@
 var Bee = function() {
+  Grub.call(this); //this refers to Object.create(Grub.prototype)
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee; //want the constructor to point back to its own function;
+
+
+
+
+
+
 
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,20 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.treasureChest = .__proto__.treasureChest;
+};
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return "I am too old, let me play cards insead";
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
 };
 
